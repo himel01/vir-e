199364eff52dc0b2cf2ea29b6de87e059ca15cd0
Aqemur Reza <aqemur.reza@gmail.com>
2024-11-24 13:03:30 +0200

    added popular banner

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Hero.css'
 import hand_icon from '../Assets/hand_icon.png'
 import arrow_icon from '../Assets/arrow.png'
+import hero_image from '../Assets/hero_image.png'
 
 export const Hero = () => {
   return (
@@ -22,7 +23,7 @@ export const Hero = () => {
             </div>
         </div>
         <div className="hero-right">
-
+            <img src={hero_image} alt="" />
         </div>
     </div>
   )
